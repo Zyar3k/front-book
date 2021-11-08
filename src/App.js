5bc8f8fd63b61f7b5cd2892819aa27ea8b3d87a4
zyar3k <a.zygan@gmail.com>
2021-11-08 20:01:36 +0100

    Init project

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>header logo</h1>
+      </header>
+      <main>
+        <h1>start</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          quis eius assumenda expedita officia, ad repudiandae ipsam, voluptates
+          totam tempore adipisci perferendis id ipsum et dolore dignissimos
+          inventore nobis, doloremque modi eligendi alias aperiam. Tenetur ex
+          aliquid sequi iusto eaque fuga, deserunt voluptatum libero accusantium
+          excepturi odio voluptas quidem modi esse, eveniet rerum temporibus rem
+          similique perferendis aut, in doloremque laudantium saepe ipsam?
+          Laudantium blanditiis magnam at sed quis similique distinctio cum
+          quibusdam quo tenetur non temporibus iure vitae repudiandae, tempora
+          cumque, accusantium praesentium, velit excepturi! Facere vel adipisci
+          vero, possimus amet ipsam error provident distinctio quia, libero et.
+          Ut!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          quis eius assumenda expedita officia, ad repudiandae ipsam, voluptates
+          totam tempore adipisci perferendis id ipsum et dolore dignissimos
+          inventore nobis, doloremque modi eligendi alias aperiam. Tenetur ex
+          aliquid sequi iusto eaque fuga, deserunt voluptatum libero accusantium
+          excepturi odio voluptas quidem modi esse, eveniet rerum temporibus rem
+          similique perferendis aut, in doloremque laudantium saepe ipsam?
+          Laudantium blanditiis magnam at sed quis similique distinctio cum
+          quibusdam quo tenetur non temporibus iure vitae repudiandae, tempora
+          cumque, accusantium praesentium, velit excepturi! Facere vel adipisci
+          vero, possimus amet ipsam error provident distinctio quia, libero et.
+          Ut!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          quis eius assumenda expedita officia, ad repudiandae ipsam, voluptates
+          totam tempore adipisci perferendis id ipsum et dolore dignissimos
+          inventore nobis, doloremque modi eligendi alias aperiam. Tenetur ex
+          aliquid sequi iusto eaque fuga, deserunt voluptatum libero accusantium
+          excepturi odio voluptas quidem modi esse, eveniet rerum temporibus rem
+          similique perferendis aut, in doloremque laudantium saepe ipsam?
+          Laudantium blanditiis magnam at sed quis similique distinctio cum
+          quibusdam quo tenetur non temporibus iure vitae repudiandae, tempora
+          cumque, accusantium praesentium, velit excepturi! Facere vel adipisci
+          vero, possimus amet ipsam error provident distinctio quia, libero et.
+          Ut!
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default App;
